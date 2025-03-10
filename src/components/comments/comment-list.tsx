8c64def32465310681e0e363a213822478f5c1b5
Waris Akinocho <warisakinocho3@gmail.com>
2025-03-10 02:01:56 +0100

    Update UI

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/contexts/auth/hook";
 import { useToast } from "@/hooks/use-toast";
 import { deleteComment, getComments } from "@/services/comments";
 import { CommentData, EditCommentPayload } from "@/types";
 import UserAvatar from "@/user-avatar";
-import { Pencil, Trash2 } from "lucide-react";
+import { Ellipsis, PencilLine, Trash2 } from "lucide-react";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 type Props = {
@@ -85,36 +86,55 @@ export default function CommentsList({ id_post, setComment, setIsUpdate }: Props
                     <span className="font-bh-medium text-sm">{comment.user.username}</span>
                     <p className="text-xs md:text-sm text-primary/80">{comment.content}</p>
                   </div>
+                  {isCommentOwner && (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          size="icon-xs"
+                          variant="ghost"
+                          className="border-none rounded-lg"
+                          onClick={() => {
+                            setComment({ id_comment: comment.id_comment, content: comment.content });
+                            setIsUpdate(true);
+                          }}
+                        >
+                          <Ellipsis className="size-4 text-primary/70" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        className="w-52 flex flex-col gap-1"
+                        align="end"
+                        forceMount
+                      >
+                        <Button
+                          variant="ghost"
+                          className="gap-2 w-full justify-start text-xs text-primary/70"
+                          onClick={() => {
+                            setComment({ id_comment: comment.id_comment, content: comment.content });
+                            setIsUpdate(true);
+                          }}
+                        >
+                          <PencilLine
+                            strokeWidth={1.8}
+                            className="size-4 text-blue-500"
+                          />
+                          Edit Comment
+                        </Button>
+                        <Button
+                          variant="destructive"
+                          className="gap-2 w-full justify-start bg-background text-xs"
+                          onClick={() => handleDeleteComment(comment.id_comment)}
+                        >
+                          <Trash2
+                            strokeWidth={1.8}
+                            className="size-4 text-destructive"
+                          />
+                          Delete Comment
+                        </Button>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  )}
                 </div>
-                {isCommentOwner && (
-                  <div className="flex justify-end gap-2">
-                    <Button
-                      size="icon"
-                      variant="outline"
-                      className="size-7 border-none rounded-lg"
-                      onClick={() => {
-                        setComment({ id_comment: comment.id_comment, content: comment.content });
-                        setIsUpdate(true);
-                      }}
-                    >
-                      <Pencil
-                        strokeWidth={1.8}
-                        className="size-4 text-blue-500"
-                      />
-                    </Button>
-                    <Button
-                      size="icon"
-                      variant="outline"
-                      className="size-7 border-none rounded-lg"
-                      onClick={() => handleDeleteComment(comment.id_comment)}
-                    >
-                      <Trash2
-                        strokeWidth={1.8}
-                        className="size-4 text-destructive"
-                      />
-                    </Button>
-                  </div>
-                )}
               </div>
             );
           })}
